dfba24b5879c13824e96760e335e9cf4cd580ec5
Matthew Davis <matc.davis@gmail.com>
2022-08-03 21:47:56 +0000

    added spinner for 8100 seconds and multiple rotations

--- spinner1.js
+++ spinner1.js
@@ -1,1 +1,30 @@
-spinner
+// spinner
+process.stdout.write("hello from spinner1.js ... \rheyyy\n") 
+let x = 100;
+while (x < 8100){
+setTimeout(() => {
+  process.stdout.write('\r|  🥱 ');
+}, x);
+x += 200;
+setTimeout(() => {
+  process.stdout.write('\r/  🥱 ');
+}, x);
+x += 200;
+setTimeout(() => {
+  process.stdout.write('\r-  🥱 ')
+}, x);
+x += 200;
+setTimeout(() => {
+  process.stdout.write('\r\\  🥱 ')
+}, x);
+x += 200;
+
+setTimeout(() => {
+  process.stdout.write('\r|  🥱')
+}, x);
+x += 200;
+
+}
+setTimeout(() => {
+  process.stdout.write(" \r 🥳 \n");
+}, x);
